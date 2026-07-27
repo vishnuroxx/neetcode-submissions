9df1a68b4e20825ef9f2356914ddcd11ebf0366c
vishnuroxx <srivishnurajkumar7@gmail.com>
2026-07-27 13:27:15 +0800

    Add: permutation-string - submission-8

---
+++ Data Structures & Algorithms/permutation-string/submission-8.py
@@ -0,0 +1,30 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        hash = {}
+        for ch in s1:
+            hash[ch] = hash.get(ch, 0) + 1
+
+        left, right = -1, 0 
+        characters = {}
+        
+        while left <= right and right < len(s2):
+            if hash.get(s2[right]):
+                if characters.get(s2[right],0) + 1 <= hash[s2[right]]:
+                    characters[s2[right]] = characters.get(s2[right],0) + 1
+                    if left == -1:
+                        left = right
+                    if right - left + 1 == len(s1):
+                        return True
+                else:
+                    characters[s2[left]] -= 1
+                    left += 1
+                    right -= 1
+            else:
+                left = -1
+                characters = {}
+            right += 1
+
+        return False
+
+
+        
