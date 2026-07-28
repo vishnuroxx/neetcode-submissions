9df8fca177b1f65233b0072047a93194b157586c
vishnuroxx <srivishnurajkumar7@gmail.com>
2026-07-28 11:20:54 +0800

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    import math
+    def isSuccessful(self, piles, k, h) -> bool:
+        if len(piles):
+            hours_taken = 0
+            for pile in piles: 
+                hours_taken += math.ceil(pile / k)
+            if hours_taken > h:
+                return False
+        return True 
+                
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left, right = 1, max(piles) 
+        while left <= right:
+            mid = (left + right) // 2
+            if self.isSuccessful(piles, mid, h):
+                if mid == 1 or not self.isSuccessful(piles, mid-1, h):
+                    return mid
+                else:
+                    right = mid - 1
+            else:
+                left = mid + 1
+
+        
